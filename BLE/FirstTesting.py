d7e867ec0eb302b4e6883ca2b9288049878c5e3b
isalan06 <isalan06@gmail.com>
2024-03-19 10:33:46 +0800

    update

--- BLE/FirstTesting.py
+++ BLE/FirstTesting.py
@@ -6,5 +6,7 @@ import bluetooth
 
 nearby_devices = bluetooth.discover_devices()
 
+print("found %d devices" % len(nearby_devices))
+
 for bdaddr in nearby_devices:
     print(bluetooth.lookup_name( bdaddr ))
