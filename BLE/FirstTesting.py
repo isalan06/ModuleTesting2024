f3136daf3214e6c74152198c28e2e90ec549dd87
isalan06 <isalan06@gmail.com>
2024-03-19 11:58:27 +0800

    update

--- BLE/FirstTesting.py
+++ BLE/FirstTesting.py
@@ -15,8 +15,8 @@ for bdaddr in nearby_devices:
 
 print("Scan BLE")
 
-service = DiscoveryService()
-devices = service.discover(0)
+service = DiscoveryService("hci0")
+devices = service.discover(2)
 
 for address, name in devices.items():
     print("name: {}, address: {}".format(name, address))
