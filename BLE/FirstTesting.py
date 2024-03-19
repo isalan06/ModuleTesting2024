fd8eb2cba25d554223fa97b0aea4c353ec6d9544
isalan06 <isalan06@gmail.com>
2024-03-19 13:45:39 +0800

    update

--- BLE/FirstTesting.py
+++ BLE/FirstTesting.py
@@ -26,14 +26,20 @@ print("Server")
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = bluetooth.get_available_port( bluetooth.RFCOMM )#1
-server_sock.bind(("",port))
+server_sock.bind(("",bluetooth.PORT_ANY))
 server_sock.listen(1)
 
-print("listening on port %d" % port)
+port = server_sock.getsockname()[1]
 
-uuid = "1e0ca4ea-299d-4335-93eb-27fcfe7fa848"
-bluetooth.advertise_service( server_sock, "FooBar Service", uuid )
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                            # protocols=[bluetooth.OBEX_UUID]
+                            )
+
+print("Waiting for connection on RFCOMM channel", port)
 
 client_sock,address = server_sock.accept()
 print("Accepted connection from ",address)
