dcfbe3c31a329beded5059e18df1221250ef6019
isalan06 <isalan06@gmail.com>
2024-03-19 13:13:38 +0800

    update

--- BLE/FirstTesting.py
+++ BLE/FirstTesting.py
@@ -20,3 +20,21 @@ devices = service.discover(2)
 
 for address, name in devices.items():
     print("name: {}, address: {}".format(name, address))
+
+# RFCOMM Server
+print("Server")
+
+server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+
+port = 1
+server_sock.bind(("",port))
+server_sock.listen(1)
+
+client_sock,address = server_sock.accept()
+print("Accepted connection from ",address)
+
+data = client_sock.recv(1024)
+print ("received [%s]" % data)
+
+client_sock.close()
+server_sock.close()
