20adddbd1d6ccc53d75bd1712bc1d8e964bb4884
isalan06 <isalan06@gmail.com>
2024-03-19 11:08:23 +0800

    update

--- BLE/FirstTesting.py
+++ BLE/FirstTesting.py
@@ -2,7 +2,9 @@
 #FirstTesting.py
 
 import bluetooth
+from bluetooth.ble import DiscoveryService
 
+print("Scan Blue Tooth")
 
 nearby_devices = bluetooth.discover_devices()
 
@@ -10,3 +12,11 @@ print("found %d devices" % len(nearby_devices))
 
 for bdaddr in nearby_devices:
     print(bluetooth.lookup_name( bdaddr ))
+
+print("Scan BLE")
+
+service = DiscoveryService()
+devices = service.discover(2)
+
+for address, name in devices.items():
+    print("name: {}, address: {}".format(name, address))
