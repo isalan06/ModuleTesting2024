99d3ce5a6c395063a22945548a13c99406e30a51
isalan06 <isalan06@gmail.com>
2024-08-01 14:05:22 +0800

    update

--- Other/watermetertesting.py
+++ Other/watermetertesting.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 #watermetertesting.py
 
+#sudo apt-get update
+#sudo apt-get install python3-serial
+#sudo apt-get install python3-can
+
 import RPi.GPIO as GPIO
 import time
+import serial
 
-wakeup_pin = 29
+RS485_EN =  4
+
+wakeup_pin = 5
 
 print("Initializing............")
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(wakeup_pin, GPIO.OUT)
 
 print("Trigger wake up HIGH.........")
@@ -22,6 +29,12 @@ GPIO.output(wakeup_pin, GPIO.LOW)
 time.sleep(0.001)
 
 print("Send Message to")
+
+GPIO.setup(RS485_EN,GPIO.OUT)
+GPIO.output(RS485_EN,GPIO.HIGH)
+
+t = serial.Serial("/dev/ttyS3",115200)    
+print(t.portstr) 
 
 
 GPIO.cleanup()
