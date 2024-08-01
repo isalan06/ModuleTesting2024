dfeea3f92fd0f346455a44241bfe9c8c95a36bca
isalan06 <isalan06@gmail.com>
2024-08-01 13:51:15 +0800

    update

--- Other/watermetertesting.py
+++ Other/watermetertesting.py
@@ -11,5 +11,20 @@ print("Initializing............")
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(wakeup_pin, GPIO.OUT)
 
+print("Trigger wake up HIGH.........")
+
+GPIO.output(wakeup_pin, GPIO.HIGH)
+time.sleep(0.005)
+
+print("Trigger wake up LOW.........")
+
+GPIO.output(wakeup_pin, GPIO.LOW)
+time.sleep(0.001)
+
+print("Send Message to")
+
+
 GPIO.cleanup()
+
+print("End of program!!!!")
 
