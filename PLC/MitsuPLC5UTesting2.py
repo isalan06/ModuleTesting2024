21f2ce5fbf7420dbed3197f798b249a9ce64355c
isalan06 <isalan06@gmail.com>
2024-12-26 12:24:48 +0800

    update

--- PLC/MitsuPLC5UTesting2.py
+++ PLC/MitsuPLC5UTesting2.py
@@ -16,7 +16,7 @@ logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-client = ModbusClient(method='rtu', port='/dev/ttyS0', baudrate=19200, timeout=3,stopbits=1, bytesize=8, parity='O')
+client = ModbusClient(method='rtu', port='/dev/ttyTHS1', baudrate=19200, timeout=3,stopbits=1, bytesize=8, parity='O')
 
 def readPLCData(client):
     try:
