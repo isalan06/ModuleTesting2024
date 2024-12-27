d9588e705512f44a0d78ca8622ca40e4ba4fe5d2
isalan06 <isalan06@gmail.com>
2024-12-27 10:09:50 +0800

    update

--- PLC/MitsuPLC5UTesting2.py
+++ PLC/MitsuPLC5UTesting2.py
@@ -10,13 +10,14 @@ from pymodbus.exceptions import ModbusException, ConnectionException
 import logging
 
 UNIT = 0x1
+serial_device = '/dev/ttyTHS0'
 
 # 配置日志记录
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
-client = ModbusClient(method='rtu', port='/dev/ttyTHS1', baudrate=19200, timeout=3,stopbits=1, bytesize=8, parity='O')
+client = ModbusClient(method='rtu', port=serial_device, baudrate=19200, timeout=3,stopbits=1, bytesize=8, parity='O')
 
 def readPLCData(client):
     try:
@@ -46,7 +47,7 @@ def readPLCData(client):
 def main():
     try:
         if client.connect():  # 尝试连接到Modbus服务器/设备
-            print("Connect to Modbus Device")
+            print(f"Connect to Modbus Device({serial_device})")
 
             d1000, d1001, d1002 = readPLCData(client)
 
